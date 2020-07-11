9f143a9a7282b341fe322bb00b080d9037dbc1c2
Ben Jones <benjones5@gmail.com>
2020-07-11 12:23:10 +0100

    Adding adjusted daily fees

--- backtrader/feeds/alpha_vantage.py
+++ backtrader/feeds/alpha_vantage.py
@@ -12,9 +12,22 @@ class AlphaVantageFeed(TimeSeries):
             key=os.environ['ALPHA_VANTAGE_KEY'],
             output_format='pandas')
 
-    def feed(self, symbol, start_date, end_date, **kwargs):
+    def get_daily_feed(self, symbol, start_date, end_date, **kwargs):
         output_size = kwargs.pop('output_size', 'full')
         data, _ = self.get_daily(symbol, outputsize=output_size)
+
+        df_data = (
+           data
+           .sort_index()
+           .rename(columns={column:column[3:] for column in data.columns})
+           .loc[start_date: end_date]
+        )
+
+        return backtrader.feeds.PandasData(dataname=df_data)
+
+    def get_daily_adjusted_feed(self, symbol, start_date, end_date, **kwargs):
+        output_size = kwargs.pop('output_size', 'full')
+        data, _ = self.get_daily_adjusted(symbol, outputsize=output_size)
 
         df_data = (
            data
